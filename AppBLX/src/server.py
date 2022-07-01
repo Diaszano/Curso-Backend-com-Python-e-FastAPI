a7a41bf855e466f8f7c073df1b70bfc7ad50bde9
Diaszano <lucasdiiassantos@gmail.com>
2022-07-01 02:40:12 -0300

    configurado o server e o alembic

--- AppBLX/src/server.py
+++ AppBLX/src/server.py
@@ -3,10 +3,11 @@
 # BIBLIOTECAS
 #-----------------------
 import sys
+from typing import List
 from src.schemas import schemas
 from sqlalchemy.orm import Session
-from fastapi import FastAPI, Depends
-from src.infra.sqlalchemy.config.database import get_db, criar_db
+from fastapi import FastAPI, Depends, status
+from src.infra.sqlalchemy.config.database import get_db
 from src.infra.sqlalchemy.repositorios.pedido import RepositorioPedido
 from src.infra.sqlalchemy.repositorios.usuario import RepositorioUsuario
 from src.infra.sqlalchemy.repositorios.produto import RepositorioProduto
@@ -22,36 +23,54 @@ app = FastAPI();
 # FUNÇÕES()
 #-----------------------
 # Usuário
-@app.get("/usuarios")
-def listar_usuarios(db:Session=Depends(get_db)):
-    lista_produtos = RepositorioUsuario(db).listar();
+@app.get(  "/usuarios",
+            status_code=status.HTTP_200_OK,
+            response_model=List[schemas.Usuario]
+        )
+def listar_usuarios(session:Session=Depends(get_db)):
+    lista_produtos = RepositorioUsuario(session).listar();
     return lista_produtos;
 
-@app.post("/usuarios",status_code=201)
-def criar_usuarios(produto:schemas.Usuario,db:Session=Depends(get_db)):
-    produto_criado = RepositorioUsuario(db).criar(produto);
+@app.post(  "/usuarios",
+            status_code=status.HTTP_201_CREATED,
+            response_model=schemas.Usuario
+        )
+def criar_usuarios(produto:schemas.Usuario,session:Session=Depends(get_db)):
+    produto_criado = RepositorioUsuario(session).criar(produto);
     return produto_criado;
 
 # Produto
-@app.get("/produtos")
-def listar_produtos(db:Session=Depends(get_db)):
-    lista_produtos = RepositorioProduto(db).listar();
+@app.get(   "/produtos",
+            status_code=status.HTTP_200_OK,
+            response_model=List[schemas.Produto]
+        )
+def listar_produtos(session:Session=Depends(get_db)):
+    lista_produtos = RepositorioProduto(session).listar();
     return lista_produtos;
 
-@app.post("/produtos",status_code=201)
-def criar_produto(produto:schemas.Produto,db:Session=Depends(get_db)):
-    produto_criado = RepositorioProduto(db).criar(produto);
+@app.post(  "/produtos",
+            status_code=status.HTTP_201_CREATED,
+            response_model=schemas.Produto
+        )
+def criar_produto(produto:schemas.Produto,session:Session=Depends(get_db)):
+    produto_criado = RepositorioProduto(session).criar(produto);
     return produto_criado;
 
 # Pedido
-@app.get("/pedidos")
-def listar_pedidos(db:Session=Depends(get_db)):
-    lista_produtos = RepositorioPedido(db).listar();
+@app.get(   "/pedidos",
+            status_code=status.HTTP_200_OK,
+            response_model=List[schemas.Usuario]
+        )
+def listar_pedidos(session:Session=Depends(get_db)):
+    lista_produtos = RepositorioPedido(session).listar();
     return lista_produtos;
 
-@app.post("/pedidos",status_code=201)
-def criar_pedidos(produto:schemas.Pedido,db:Session=Depends(get_db)):
-    produto_criado = RepositorioPedido(db).criar(produto);
+@app.post(  "/pedidos",
+            status_code=status.HTTP_201_CREATED,
+            response_model=List[schemas.Usuario]
+        )
+def criar_pedidos(produto:schemas.Pedido,session:Session=Depends(get_db)):
+    produto_criado = RepositorioPedido(session).criar(produto);
     return produto_criado;
 #-----------------------
 # Main()
